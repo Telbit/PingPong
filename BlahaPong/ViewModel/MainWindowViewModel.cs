e6a904b47aed2431a5d80420c48cc97491797c96
J <J@DESKTOP-K8E0BDM>
2020-10-28 16:20:47 +0100

    game counter and refactored canvas element adding, moved from MainWindow.cs to MainWindowViewModel constructor

--- BlahaPong/ViewModel/MainWindowViewModel.cs
+++ BlahaPong/ViewModel/MainWindowViewModel.cs
@@ -14,17 +14,57 @@ namespace BlahaPong.ViewModel
 {
     public class MainWindowViewModel
     {
-        public Paddle playerOne { get; } = new Paddle(20, 115, 5, 200, 10);
-        public Paddle playerTwo { get; } = new Paddle(750, 115, 5, 200, 10);
-        public Ball _ball { get; } = new Ball(380, 197, 5, 20, 20);
+        public MainWindowViewModel(Canvas canv)
+        {
+            canv.Children.Add(playerOne.Rectangle);
+            canv.Children.Add(playerTwo.Rectangle);
+            canv.Children.Add(_ball.BallItem);
+            canv.Children.Add(PauseImage);
+            canv.Children.Add(PlayerOneScore);
+            canv.Children.Add(PlayerTwoScore);
+        }
 
+        private readonly static int SCORE_BOX_HEIGHT = 45;
 
-        public Image PauseImage { get; } = new Image()
+        private readonly static int SCORE_BOX_WIDTH = 46;
+
+        private readonly static int SCORE_BOX_FONT_SIZE = 36;
+
+        private readonly static string SCORE_BOX_BASICS_TEXT = "00";
+        
+        private readonly static FontFamily SCORE_BOX_FONT_FAMILY = new FontFamily("Bahnschrift SemiBold");
+
+        private Paddle playerOne { get; } = new Paddle(20, 115, 5, 200, 10);
+        private Paddle playerTwo { get; } = new Paddle(750, 115, 5, 200, 10);
+        private Ball _ball { get; } = new Ball(380, 197, 5, 20, 20);
+
+        private TextBox PlayerOneScore { get; set; } = new TextBox() {
+            Height = SCORE_BOX_HEIGHT,
+            Width = SCORE_BOX_WIDTH,
+            Text= SCORE_BOX_BASICS_TEXT,
+            TextWrapping = TextWrapping.Wrap,
+            FontSize = SCORE_BOX_FONT_SIZE,
+            FontFamily = SCORE_BOX_FONT_FAMILY,
+            IsReadOnly = true,
+            BorderThickness = new Thickness(0)
+        };
+        private TextBox PlayerTwoScore { get; set; } = new TextBox() {
+            Height = SCORE_BOX_HEIGHT,
+            Width = SCORE_BOX_WIDTH,
+            Text = SCORE_BOX_BASICS_TEXT,
+            TextWrapping = TextWrapping.Wrap,
+            FontSize = SCORE_BOX_FONT_SIZE,
+            FontFamily = SCORE_BOX_FONT_FAMILY,
+            IsReadOnly = true,
+            BorderThickness = new Thickness(0)
+        };
+
+        private Image PauseImage { get; } = new Image()
         {
             Height = 206,
             Width = 708,
             Visibility = Visibility.Hidden,
-            Source = new BitmapImage(new Uri("C:/Users/J/source/repos/c-sharp-pingpong-blahapong/BlahaPong/Resources/pauseTwo.png"))
+            Source = new BitmapImage(new Uri("C:/Users/J/source/repos/c-sharp-pingpong-blahapong/BlahaPong/Resources/pause.png"))
         };
         
         private DispatcherTimer timer;
@@ -66,6 +106,9 @@ namespace BlahaPong.ViewModel
             }
         }
         ExitWindowViewModel _exitWindowViwModel = new ExitWindowViewModel();
+
+        
+
         //PauseWindowViewModel _pauseWindowViwModel = new PauseWindowViewModel();
 
         public void ShowExitMessageBox()
@@ -105,6 +148,13 @@ namespace BlahaPong.ViewModel
         {
             Canvas.SetLeft(PauseImage, 46);
             Canvas.SetTop(PauseImage, 104);
+            
+            Canvas.SetLeft(PlayerOneScore, 338);
+            Canvas.SetTop(PlayerOneScore, 10);
+            
+            Canvas.SetLeft(PlayerTwoScore, 412);
+            Canvas.SetTop(PlayerTwoScore, 10);
+            
             timer = new DispatcherTimer();
             timer.Tick += UpdateGame;
             timer.Interval = new TimeSpan(0, 0, 0,0,25);
